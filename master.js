99c67c5d47c40390bea600ac218a43da06bf1cff
oussamakenzi <oussamakenzi.edu@gmail.com>
2024-03-29 15:21:13 +0000

    modifcation sur le formule est lafichage de les cantact

--- master.js
+++ master.js
@@ -1,8 +1,11 @@
 function toggleForm() {
+    let show = document.querySelector(".show-account");
     let form = document.getElementById("form");
-    if (form.style.display === "none") {
+    if (form.style.display === "none" && show.style.display === "block") {
         form.style.display = "block";
+        show.style.display = "none";
     } else {
         form.style.display = "none";
+        show.style.display = "block"; // Ajoutez ceci si vous voulez afficher .show-account lorsque vous masquez le formulaire.
     }
 }
