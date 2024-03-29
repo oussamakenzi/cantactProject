e05815519345d2304496cac554652d5d1c1e4c70
oussamakenzi <oussamakenzi.edu@gmail.com>
2024-03-29 15:08:12 +0000

    formule dajoute une cantact

---
+++ master.js
@@ -0,0 +1,8 @@
+function toggleForm() {
+    let form = document.getElementById("form");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
